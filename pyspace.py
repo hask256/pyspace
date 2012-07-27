9850bfab6557dd1f32d4c969d851a8b776c47a9b
hask <hask256@gmail.com>
2012-07-27 17:27:37 +0200

    music, life, collision

--- pyspace.py
+++ pyspace.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame import sprite
 import time
 from random import randint
 
@@ -10,6 +11,7 @@ Color = namedtuple('Color', 'red green blue')
 black = Color(0,   0,    0)
 white = Color(255,255, 255)
 blue  = Color(0,   0,  255)
+green = Color(0, 140,    0)
 
 
 screen_size = Size(800, 600)
@@ -28,7 +30,20 @@ class Spaceship(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+        x = self.rect.x
+        y = self.rect.y
+        w = screen_size.w - self.rect.w
+        h = screen_size.h - self.rect.h
 
+        if x > w: self.rect.x = w
+        if x < 0: self.rect.x = 0
+
+        if y > h: self.rect.y = h
+        if y < 0: self.rect.y = 0
+
+    def move_to_pos(self, pos):
+        self.rect.x = pos.x
+        self.rect.y = pos.y
 
 class Alien(pygame.sprite.Sprite):
 
@@ -44,6 +59,7 @@ class Alien(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+
 class Fire(pygame.sprite.Sprite):
 
     def __init__(self, point, image):
@@ -53,6 +69,9 @@ class Fire(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = point
+
+    def on_board(self, w, h):
+        return (0 < self.rect.x < w) and (0 < self.rect.y < h)
 
         
 
@@ -82,14 +101,27 @@ class RandFire(Fire):
         super(RandFire, self).__init__(point, "fire.png")
 
     def move(self):
-        self.rect.x += randint(-2, 2)
+        self.rect.x += randint(-3, 3)
         self.rect.y -= 5
 
 
 
+class Bonus(pygame.sprite.Sprite):
 
+    def __init__(self, point):
+        pygame.sprite.Sprite.__init__(self)
 
+        self.image = pygame.image.load("bonus.png").convert()
 
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = point
+
+class Map(object):
+    def __init__(self, alien, bonus):
+        self.alien = alien
+        self.bonus = bonus
+        
+    
 def random_color():
     return Color(randint(0, 255), randint(0, 255), randint(0, 255))
 
@@ -127,25 +159,42 @@ fire_keys = {pygame.K_z: LaserFire,
 pygame.key.set_repeat(5, 5)
 
 
-fires = pygame.sprite.RenderPlain()
-aliens = pygame.sprite.RenderPlain()
+fires = pygame.sprite.Group()
+aliens = pygame.sprite.Group()
+bonus = pygame.sprite.Group()
 
 
 
 points = 0
+killed = 0
+points = 0
+life = 5
 start_time = time.time()
 
-while not end:
-    clock.tick(30)
-    screen.fill(black)
+def set_labels():
 
+    myfont = pygame.font.SysFont("", 25)
 
-    for f in fires:
-        f.move()
+    title = ["time: %s"    % int(time.time() - start_time),
+             "fire: %s"    % len(fires),
+             "enemies: %s" % len(aliens),
+             "%s : %s"     % (player.rect.x, player.rect.y),
+             "killed: %s"  % killed,
+             "points: %s"  % points,
+             "life:  %s"   % life
+            ]
+
+    pos = 0
+
+    for t in title:
+        screen.blit(myfont.render(t, 1, green), (0, pos))
+        pos += 20
+
+def event_handle():
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            end = True
+            return True
         if event.type == pygame.KEYDOWN:
             if event.key in moves:
                 mv, x, y = moves[event.key]
@@ -164,30 +213,56 @@ while not end:
 
                 fires.add(left_fire)
                 fires.add(right_fire)
-                points += 1
+                points = len(fires)
                 
-
             if event.key == pygame.K_q:
                 x = randint(100, screen_size.w - 100)
                 y = randint(100, screen_size.h - 100)
                 aliens.add(Alien(Point(x, y)))
 
+            if event.key == pygame.K_w:
+                x = randint(100, screen_size.w - 100)
+                y = randint(100, screen_size.h - 100)
+                bonus.add(Bonus(Point(x, y)))
 
+    return False
 
-    sprites.draw(screen)
-    fires.draw(screen)
-    aliens.draw(screen)
+sound = pygame.mixer.Sound("explosion.wav")
 
-    myfont = pygame.font.SysFont("", 25)
+while not end:
+    clock.tick(30)
+    screen.fill(black)
 
-    time_label = myfont.render(str(int(time.time() - start_time)), 1, blue)
-    points_label = myfont.render(str(points), 1, blue)
-    pos_label = myfont.render("%s : %s" % (player.rect.x, player.rect.y), 1, blue)
+    to_del = []
+    for f in fires:
+        if not f.on_board(*screen_size):
+            to_del.append(f)
+        f.move()
+        
+        if pygame.sprite.spritecollide(f, aliens, True):
+            to_del.append(f)
+            killed += 1
+            sound.play()
+
     
-    screen.blit(time_label,   (0, 0))
-    screen.blit(points_label, (0,30))
-    screen.blit(pos_label, (0, 60))
+    if pygame.sprite.spritecollide(player, bonus, True):
+        points += 100
 
+    if pygame.sprite.spritecollide(player, aliens, False):
+        life -= 1
+        player.move_to_pos(Point(300, 500))
+
+            
+
+    for d in to_del:
+        fires.remove(d)
+
+    end = event_handle()
+
+    for s in sprites, fires, aliens, bonus:
+        s.draw(screen)
+    
+    set_labels()
 
     pygame.display.flip()
 
